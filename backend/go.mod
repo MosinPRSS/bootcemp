b1a7b2a14a6b234ba181abb1f1253b2aa62e9b62
n3cr0l0rd <addarol@protonmail.com>
2026-09-08 14:59:50 +0300

    Added core server logic

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,13 @@ module bootcemp/server
 go 1.27.1
 
 require (
+	github.com/lib/pq v1.12.3
+	golang.org/x/sync v0.22.0
+	gorm.io/gorm v1.31.2
+)
+
+require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/lib/pq v1.12.3 // indirect
 	golang.org/x/text v0.41.0 // indirect
-	gorm.io/gorm v1.31.2 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,3 +1,40 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	wg, _ := errgroup.WithContext(context.Background())
+	mux := http.NewServeMux()
+
+	infLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	const addr = "127.0.0.1:4308"
+
+	srv := &http.Server{
+		Addr:     addr,
+		ErrorLog: errLogger,
+		Handler:  mux,
+	}
+
+	wg.Go(func() error {
+		infLogger.Printf("Attempting to bind on %s", addr)
+
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			return err
+		}
+		return nil
+	})
+
+	infLogger.Printf("Starting HTTP server %s", addr)
+	if err := wg.Wait(); err != nil {
+		errLogger.Fatalf("Error starting HTTP server on %s: %s", addr, err)
+	}
+}
